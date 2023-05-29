795613ab133957edf3aff043aa172139e4eae105
Braden Godley <braden@bgodley.com>
2023-05-29 15:15:19 -0700

    design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,91 +4,165 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <nav className={[styles.container, styles.nav].join(" ")}>
+        <div className={styles.horizontal_stack}>
+          <div className={styles.horizontal_stack}>
+            <Image src="/logo.png" alt="logo" width="50" height="50" style={{
+              transform: "translateY(15px)"
+            }} />
+            <h2>
+              Arcenergy
+            </h2>
+          </div>
+          <div className={styles.horizontal_stack} style={{
+            paddingLeft: "2rem"
+          }}>
+            <a className={styles.nav_link}>About</a>
+            <a className={styles.nav_link}>Platform</a>
+            <a className={styles.nav_link}>Contact</a>
+          </div>
         </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div className={styles.horizontal_stack}>
+          <a className={styles.nav_link}>Login</a>
+        </div>
+      </nav>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      <div className={styles.container}>
+        <div className={styles.col_2} style={{
+          aspectRatio: "2 / 1"
+        }}>
+          <div className={styles.col_left}>
+            <h1>Can climate change effects be stopped?</h1>
+            <h2>
+              All our methodology opens up a cleaner path forward for every everyday consumers.
+            </h2>
+            <div className={styles.horizontal_stack} style={{
+              justifyContent: "flex-start"
+            }}>
+              <button>Get started</button>
+              <button>Play</button>
+              <p>
+                {"Let's see how we did it."}
+              </p>
+            </div>
+          </div>
+          <div className={styles.col_right}>
+            <div
+              style={{
+                position: "absolute",
+                top: "0",
+                left: "20%",
+                width: "35%",
+                aspectRatio: "1 / 1",
+                border: "1px solid #fff",
+                padding: "1rem",
+                zIndex: 1
+              }}
+            >
+              <h2>
+                20%
+              </h2>
+              <p>Increase</p>
+              <p>Clean energy</p>
+            </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+            <div
+              style={{
+                position: "absolute",
+                left: "0",
+                top: "22.5%",
+                borderRadius: "100px",
+                backgroundColor: "#ddef46",
+                padding: "1rem 2rem",
+                color: "#111",
+                zIndex: 2
+              }}
+            >
+              <p>40% less energy</p>
+            </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+            <div
+              style={{
+                position: "absolute",
+                left: "45%",
+                right: "10%",
+                top: "10%",
+                bottom: "20%",
+                backgroundColor: "#000",
+                zIndex: 3
+              }}
+            >
+              <div style={{
+                position: "absolute",
+                top: "-10%",
+                left: "0",
+                right: "0",
+                bottom: "0"
+              }}>
+                <Image src="/man.png" alt="test" fill style={{ objectFit: "cover" }} />
+              </div>
+            </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+            <div style={{
+              position: "absolute",
+              right: "5%",
+              bottom: "25%",
+
+              width: "10%",
+              height: "10%",
+              backgroundColor: "#fff",
+              borderRadius: "100px",
+              display: "flex",
+              padding: "1rem",
+              justifyContent: "center",
+              alignItems: "center",
+              zIndex: 4
+            }}>
+              <div style={{
+                position: "relative",
+                width: "100%",
+                height: "100%"
+              }}>
+                <Image src="/star.png" alt="star" fill/>
+              </div>
+            </div>
+
+            <div
+              style={{
+                position: "absolute",
+                left: "0",
+                width: "45%",
+                top: "55%",
+                height: "45%",
+                backgroundColor: "#fff",
+                zIndex: 3,
+                color: "#111",
+                padding: "2rem"
+              }}
+            >
+              <Image src="/leaf-icon.png" alt="leaf" width="50" height="50" />
+              <h2 style={{
+                marginTop: "1rem"
+              }}>
+                New energy with proven zero pollution
+              </h2>
+            </div>
+
+            <div
+              style={{
+                position: "absolute",
+                top: "10%",
+                left: "92.5%",
+                right: "2.5%",
+                bottom: "20%",
+                writingMode: "vertical-lr"
+              }}
+            >
+              Read success stories
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   )
